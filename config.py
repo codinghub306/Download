7a252a3dcbcbae4571eefad0bc5ba1f6553f41c4
Heaven <154735143+codinghub306@users.noreply.github.com>
2025-06-14 18:26:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "20928172"
+api_hash = "48ed56c8db54f85d232f576b150360ef"
+bot_token = "7662487236:AAEP6V07GMIFxFWSCjfKLpL1Sz59QOM23kU"
+auth_users = [5810663795]
+sudo_users = [5810663795]
